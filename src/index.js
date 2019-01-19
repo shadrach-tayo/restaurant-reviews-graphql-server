e0a5d5c6bb603ec621eee2f87eb8eff59f58a39c
Shadrach Temitayo <shadrachtemitayo@gmail.com>
2019-01-19 18:47:59 +0100

    serve client from graphql server

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import path from "path";
 require("dotenv").config();
 const { ApolloServer } = require("apollo-server-express");
 import resolvers from "./resolvers";
@@ -7,7 +8,7 @@ import typeDefs from "./typeDefs";
 import Restaurant from "./models/restaurant";
 import Review from "./models/review";
 
-const { PORT = 3000, NODE_ENV = "development", DB_PASS, DB_USER } = process.env;
+const { port = 3000, NODE_ENV = "development", DB_PASS, DB_USER } = process.env;
 
 mongoose.connect(
   `mongodb://${DB_USER}:${DB_PASS}@ds245755.mlab.com:45755/restaurant-reviews`
@@ -17,6 +18,7 @@ const IN_PROD = NODE_ENV === "production";
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, "../client/dist")));
 const server = new ApolloServer({
   typeDefs,
   resolvers,
@@ -27,8 +29,8 @@ const server = new ApolloServer({
 
 server.applyMiddleware({ app });
 
-app.listen({ port: PORT }, () =>
+app.listen({ port }, () =>
   console.log(
-    `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+    `🚀 Server ready at http://localhost:${port}${server.graphqlPath}`
   )
 );
